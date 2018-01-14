7fe8a836859257b513c5deb316698827845a36c6
Ulrich Eck <ueck@net-labs.de>
2018-01-14 19:54:48 +0100

    initial draft for conan build

---
+++ conanfile.py
@@ -0,0 +1,46 @@
+from conans import ConanFile, CMake, tools
+
+
+class UbitrackCoreConan(ConanFile):
+    name = "ubitrack_component_vision"
+    version = "1.3.0"
+
+    description = "Ubitrack Core Components"
+    url = "https://github.com/Ubitrack/component_vision.git"
+    license = "GPL"
+
+    short_paths = True
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    requires = (
+        "ubitrack_core/%s@ulricheck/stable" % version,
+        "ubitrack_vision/%s@ulricheck/stable" % version,
+        "ubitrack_dataflow/%s@ulricheck/stable" % version,
+       )
+
+    default_options = (
+        "ubitrack_core:shared=True",
+        "ubitrack_vision:shared=True",
+        "ubitrack_dataflow:shared=True",
+        )
+
+    # all sources are deployed with the package
+    exports_sources = "doc/*", "src/*", "CMakeLists.txt"
+
+    def imports(self):
+        self.copy(pattern="*.dll", dst="bin", src="bin") # From bin to bin
+        self.copy(pattern="*.dylib*", dst="lib", src="lib") 
+        self.copy(pattern="*.so*", dst="lib", src="lib") 
+       
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        cmake.install()
+
+    def package(self):
+        pass
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
